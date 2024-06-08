7b63af22e185b1a5f94687e34e59ad8c637cf15a
MayMei <notalomei@gmail.com>
2024-06-08 02:35:34 -0500

    changed fire desc

--- src/components/Orbit/index.jsx
+++ src/components/Orbit/index.jsx
@@ -17,7 +17,7 @@ const [descMarkAct, setDescMarkAct] = React.useState(false);
 const [activated, setActivated] = React.useState(false);
 const [info, setInfo] = React.useState(["element-icon", "element-name", "element-desc"])
 
-const fireDes = "To cast Fire fey spells you must focus it until it feels as if you are burning.  Once done, using a wand or staff, you are able to direct the flow of your fey through it with the aid of the Feystone inside of the weapon. If your weapon of choice is a melee weapon then the focus of your fey doesn't turn into a spell to be cast out, but to be an enhancement of sorts to cover the lethal component to aid in its damaging capabilities. Can and will cause fire to soft fabric like clothing.";
+const fireDes = "To cast fire, you must focus your fey until it feels as if you are burning.  Once done, using a wand or staff, you are able to direct the flow of your fey through it with the aid of the Feystone inside of the weapon. If your weapon of choice is a melee weapon then the focus of your fey doesn't turn into a spell to be cast out, but to be an enhancement of sorts to cover the lethal component to aid in its damaging capabilities. Can and will cause fire to soft fabric like clothing. Fire is considered the default fey to cast as it's the easiest to start up, but fire doesn't have any additional effects besides setting fire to anything flammable. It also has a resistance to Ice magic. Any slowing effects what would happen due to Ice magic is cancelled out. However, it has a natural weakness to nature magic because it can effectively smother your flame out.";
 const iceDes = "While usiong Ice spells you will cause freezing damage and have a chance to slow your opponents movements down. First you focus your fey until it feels as if you are freezing. The air around you will start to condense and it will appear to give you a misty aura. You will feel cold and slower, but it is only a feeling. Ice magic has a natural immunity to nature magic as is shown by the weather here on Avolina. Ice and snow tend to overtake the land and wither all the nature down. However, Ice has a weakness to Fire.";
 const natureDes = "Fey allows users to manipulate the elements of the planet itself as it's weapon. The ground, the grass, the trees, the wind. It's known to be the hardest to start learning and the hardest to master. To cast you must focus your fey. It will feel as if the air around you is lifting you from the ground. The plants around you will start to stand up straighter as if they were being rejuvenated with fresh energy. Nature magic has a resistance to fire magic. Able to brush away a flame withe the flick of the wind or smother it with earth itself. Hoever, it does have a natural weakness to Ice. Ice can shear straight through the wind and cause the trees and grass to whither.";
 const kineticDes = "Once you've attained the ability to cast Divine Magic, all other magic becomes second nature. Kinetic is the ability to not only move objects around with ones mind, but also to teleport great distances as well as telepathy.";
@@ -34,12 +34,6 @@ const necromancyDes = "Necromancy is the opposite of Conjuration. Instead of cre
                 !descMarkAct && ( <div className="center closed-eye"></div> )
             }
 
-            {/* <div className={`center ${descMarkAct ? "opened-eye":"closed-eye"}`}> */}
-                {/* <p>Elements</p> */}
-                {/* <div className={`center-thing ${activated? "opened-eye":"closed-eye"}`}>
-                </div> */}
-                {/* <img className="center-thing" src={descMarkAct ? openedEye : closedEye} alt="" /> */}
-            {/* </div> */}
             <ul className="orbitUL">
                 <li className="element-items bg-[#ea7838]">
                     <img className="element-image" src={FireIcon} alt=""/>
